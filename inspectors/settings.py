11a5e39ec99854f3bad05e208185ac5cd9d3e6c7
Benjamin Golder <benjamin.j.golder@gmail.com>
2015-10-07 19:49:18 -0400

    Database cleanup and error fixing
    - removed dev.db, an apparent sqlite db hanging out in the repo
    - took migrations out of source control for now
    - fixed settings.py error caused by changing `SQL_DATABASE_URI`

--- inspectors/settings.py
+++ inspectors/settings.py
@@ -6,7 +6,8 @@ os_env = os.environ
 
 class Config(object):
     SECRET_KEY = os_env.get('INSPECTORS_SECRET', 'secret-key')  # TODO: Change me
-    SQLALCHEMY_DATABASE_URL = os_env['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os_env.get('DATABASE_URL',
+            'postgresql://localhost/mdc_inspectors')
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
@@ -20,8 +21,6 @@ class ProdConfig(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URL = os_env.get('DATABASE_URL',
-            'postgresql://localhost/mdc_inspectors')
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
@@ -29,8 +28,6 @@ class DevConfig(Config):
     """Development configuration."""
     ENV = 'dev'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URL = os_env.get('DATABASE_URL',
-            'postgresql://localhost/mdc_inspectors')
     DEBUG_TB_ENABLED = True
     ASSETS_DEBUG = True  # Don't bundle/minify static assets
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
@@ -39,6 +36,6 @@ class DevConfig(Config):
 class TestConfig(Config):
     TESTING = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URL = 'sqlite://'
+    SQLALCHEMY_DATABASE_URI = os_env['TEST_DATABASE_URL']
     BCRYPT_LOG_ROUNDS = 1  # For faster tests
     WTF_CSRF_ENABLED = False  # Allows form testing
